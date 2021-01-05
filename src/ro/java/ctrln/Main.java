dea0c1658c57d8ef5b66b28d47f900fb201ca073
irina <irina.spiridon_27@yahoo.com>
2021-01-05 22:34:59 +0200

    Adaugare JavaDoc

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,8 +1,12 @@
 package ro.java.ctrln;
 
 public class Main {
-    //JAR - Java Archive
-        //Java_Home
+    /**
+     * Metoda main este metoda apelata de masina virtuala Java si care porneste o aplicatie Java
+     * Putem sa transmitem argumente aditionale prin parametrul <b> args</b>
+     * @param args Acesta este parametrul cu care transmitem argumente programului nostru
+     * @author Irina Sirbu
+     */
     public static void main(String[] args) {
             System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
